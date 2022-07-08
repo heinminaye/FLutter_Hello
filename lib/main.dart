159c18c0bca2ac05c6d6036a70e2d7391eb82468
heinminaye <heinminaye8000@gmail.com>
2022-07-08 10:39:20 +0630

    'master

--- lib/main.dart
+++ lib/main.dart
@@ -16,87 +16,63 @@ class MyApp extends StatelessWidget {
           title: const Text("Hello"),
         ),
         body: Center(
-          child: Container(
-            decoration: const BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-            width: 300,
-            height: 400,
-            child: Column(children: [
-              const SizedBox(height: 30),
-              const Image(
-                image: AssetImage('images/circle.png'),
-                width: 120,
-                height: 120,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                "Olivia Mary",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text("Description", style: TextStyle(fontSize: 14)),
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                  child: IntrinsicWidth(
-                child: Column(children: [
-                  Container(
-                    height: 30,
-                    decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(5))),
-                    child: Row(children: const [
-                      Padding(
-                        padding: EdgeInsets.only(left: 15),
-                      ),
-                      Icon(Icons.phone),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Text(
-                        '09-123456789',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ]),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  Container(
-                    height: 30,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
+          child: Card(
+            color: Colors.lightBlue,
+            elevation: 5,
+            child: SizedBox(
+              width: 320,
+              height: 420,
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                const SizedBox(height: 30),
+                const Image(
+                  image: AssetImage('images/circle.png'),
+                  width: 120,
+                  height: 120,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Olivia Mary",
+                  style: TextStyle(
+                      fontFamily: "EduSA",
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text("Description", style: TextStyle(fontSize: 14)),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                    child: IntrinsicWidth(
+                  child: Column(children: const [
+                    Card(
+                      child: ListTile(
+                          leading: Icon(Icons.phone),
+                          title: Text(
+                            '09-123456789',
+                            style: TextStyle(fontSize: 16),
+                          )),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.only(left: 15),
-                        ),
-                        Icon(Icons.email),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Text(
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.email),
+                        title: Text(
                           'heinminaye@gmail.com',
                           style: TextStyle(fontSize: 16),
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(right: 15),
-                        ),
-                      ],
-                    ),
-                  )
-                ]),
-              )),
-            ]),
+                      ),
+                    )
+                  ]),
+                )),
+              ]),
+            ),
           ),
         ),
       ),
